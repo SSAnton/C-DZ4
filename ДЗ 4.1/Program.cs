2df0f4ffbdeb23b5f70b1f4aa5c6dff4a9f41f90
Anton Silantev <silantev.anton.sergeevich@gmail.com>
2023-02-24 23:52:36 +0400

    Выполнение ДЗ №4.1

---
+++ ДЗ 4.1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите метод, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// Math.Pow использовать нельзя
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите степень: ");
+int power = Convert.ToInt32(Console.ReadLine());
+
+int Power (int num, int pow) 
+{
+    int result = 1;
+    for (int i = 0; i<pow; i++) 
+    {
+        result = result * num;
+    }
+
+    return result;
+}
+
+int res = Power(number, power);
+Console.WriteLine(res);

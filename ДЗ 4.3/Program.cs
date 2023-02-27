dbc452f4640ddc855f3f84884a51766fcf6a0fde
Anton Silantev <silantev.anton.sergeevich@gmail.com>
2023-02-28 00:43:21 +0400

    Выполнение ДЗ 4.3

---
+++ ДЗ 4.3/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите метод, который задаёт массив из N элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[n];
+
+void FillArray(int[] arr)
+{
+    int len = arr.Length;
+    int i = 0;
+    while (i<len)
+    {
+        arr[i] = new Random().Next(1,100);
+        i++;
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    int len = arr.Length;
+    int i = 0;
+    while (i<len)
+    {
+        Console.WriteLine(arr[i]);
+        i++;
+    }
+}
+
+FillArray(array);
+PrintArray(array);

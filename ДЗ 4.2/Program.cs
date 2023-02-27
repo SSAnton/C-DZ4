07ddbf9467f76eabf79029d1f96108d4ea0f7b52
Anton Silantev <silantev.anton.sergeevich@gmail.com>
2023-02-28 00:10:09 +0400

    Выполнение ДЗ №4.2

---
+++ ДЗ 4.2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int DigitSum (int num) 
+{
+    int result = 0;
+
+    while (num>0)
+    {
+        result = result + num%10;
+        num = num/10;
+    }
+    return result;
+}
+
+int res = DigitSum(number);
+Console.WriteLine(res);
